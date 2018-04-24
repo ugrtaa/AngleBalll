c29e668f9b7eeea6644cf0375e55ea45cbe1b4ff
ugrtaa <ugurtahakuran@gmail.com>
2018-04-25 01:57:04 +0300

    Initial commit

---
+++ Assets/CreateFirstTransformInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFirstTransformInfo : MonoBehaviour {
+	public GameObject Line;
+	public static List<Vector3> pointList= new List<Vector3>();
+
+	private float maxLineLenght;
+	private bool isItFirstDot=true; 
+	private Vector3 secondDotpos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			var Mousepos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Mousepos.z = 0;
+			pointList.Add (Mousepos);
+			if(pointList.Count>1)
+			maxLineLenght = Mathf.Sqrt (Mathf.Abs (((pointList [0].x * pointList [0].x) - (pointList [pointList.Count - 1].x * pointList [pointList.Count - 1].x)) + ((pointList [0].y * pointList [0].y) - (pointList [pointList.Count - 1].y * pointList [pointList.Count - 1].y))));
+		}
+		if (maxLineLenght <= 5) {
+			secondDotpos = pointList [1];
+		}
+		if (maxLineLenght > 5) {
+			var mvalue = (pointList [0].y - pointList [1].y) / (pointList [0].x - pointList [1].x);
+			var mangle = Mathf.Atan (mvalue);
+			secondDotpos=new Vector3(pointList[0].x+(5*Mathf.Sin(mangle)),(pointList[0].y+(5*Mathf.Cos(mangle))),0);
+		}
+		Debug.Log (maxLineLenght);
+		if (isItFirstDot == true) {
+			Instantiate (Line, pointList[0], Quaternion.identity);
+			isItFirstDot = false;
+		}
+		if (isItFirstDot == false &&pointList.Count>1) {
+			Instantiate (Line, secondDotpos, Quaternion.identity);
+			pointList.Clear();
+			isItFirstDot = true;
+		}
+	}
+}
