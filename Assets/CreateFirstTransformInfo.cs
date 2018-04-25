976b1b900e95442b4b1ddea1127b301c507e7f85
Apo <abdurrahmangkc@outlook.com>
2018-04-25 03:23:49 +0300

    tanjant degisikliki

--- Assets/CreateFirstTransformInfo.cs
+++ Assets/CreateFirstTransformInfo.cs
@@ -20,15 +20,16 @@ public class CreateFirstTransformInfo : MonoBehaviour {
 			var Mousepos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Mousepos.z = 0;
 			pointList.Add (Mousepos);
-			if(pointList.Count>1)
-			maxLineLenght = Mathf.Sqrt (Mathf.Abs (((pointList [0].x * pointList [0].x) - (pointList [pointList.Count - 1].x * pointList [pointList.Count - 1].x)) + ((pointList [0].y * pointList [0].y) - (pointList [pointList.Count - 1].y * pointList [pointList.Count - 1].y))));
-		}
-		if (maxLineLenght <= 5) {
+            if (pointList.Count > 1) ;
+            maxLineLenght = Mathf.Sqrt(Mathf.Pow((pointList[0].x - pointList[1].x), 2) + Mathf.Pow((pointList[0].y - pointList[1].y), 2));
+        }
+        //Mathf.Abs (((pointList [0].x * pointList [0].x) - (pointList [pointList.Count - 1].x * pointList [pointList.Count - 1].x)) + ((pointList [0].y * pointList [0].y) - (pointList [pointList.Count - 1].y * pointList [pointList.Count - 1].y)))
+        if (maxLineLenght <= 5) {
 			secondDotpos = pointList [1];
 		}
 		if (maxLineLenght > 5) {
-			var mvalue = (pointList [1].y - pointList [0].y) / (pointList [1].x - pointList [0].x);
-			var mangle = Mathf.Atan (mvalue);
+			//var mvalue = (pointList [1].y - pointList [0].y) / (pointList [1].x - pointList [0].x);
+			var mangle = Mathf.Atan2 ((pointList[1].y - pointList[0].y), (pointList[1].x - pointList[0].x));
 			secondDotpos=new Vector3(pointList[0].x+(5*Mathf.Cos(mangle)),(pointList[0].y+(5*Mathf.Sin(mangle))),0);
 		}
 		Debug.Log (maxLineLenght);
