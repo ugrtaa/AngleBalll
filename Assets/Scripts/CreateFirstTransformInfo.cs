e8ac875e9c0300ef9ee9d903a437ed441eb97ac2
ugrtaa <ugurtahakuran@gmail.com>
2018-05-01 00:27:58 +0300

    DotAndLaserCompleted

--- Assets/Scripts/CreateFirstTransformInfo.cs
+++ Assets/Scripts/CreateFirstTransformInfo.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class CreateFirstTransformInfo : MonoBehaviour {
 	public GameObject Line;
+	public GameObject LineLaser;
 	public static List<Vector3> pointList= new List<Vector3>();
+	public static float rot;
+
 
 	private float maxLineLenght;
 	private bool isItFirstDot=true; 
 	private Vector3 secondDotpos;
 	private bool isMousePressed=false;
+	private GameObject Dot1,Dot2;
+	private Vector3 DotPosition;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,6 +22,7 @@ public class CreateFirstTransformInfo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log (rot);
 		if (Input.GetMouseButtonDown (0)) {
 			var Mousepos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Mousepos.z = 0;
@@ -34,17 +40,47 @@ public class CreateFirstTransformInfo : MonoBehaviour {
 				var mangle = Mathf.Atan2 ((pointList [1].y - pointList [0].y), (pointList [1].x - pointList [0].x));
 				secondDotpos = new Vector3 (pointList [0].x + (2 * Mathf.Cos (mangle)), (pointList [0].y + (2 * Mathf.Sin (mangle))), 0);
 			}
-			Debug.Log (maxLineLenght);
 			if (isItFirstDot == true) {
-				Instantiate (Line, pointList [0], Quaternion.identity);
+				Dot1=Instantiate (Line, pointList [0],Quaternion.identity) as GameObject;
+				DotPosition = Dot1.transform.position;
+				DotPosition.z = 1;
+
+				Debug.Log (DotPosition);
 				isItFirstDot = false;
 			}
-			if (isItFirstDot == false) {
-				Instantiate (Line, secondDotpos, Quaternion.identity);
-				pointList.Clear ();
+			if (isItFirstDot == false && pointList.Count > 1) {
+				Dot2=Instantiate (Line, secondDotpos, Quaternion.identity) as GameObject;
 				isItFirstDot = true;
 				isMousePressed = false;
 			}
+
+		}
+		DegreeCalculate ();
+	}
+	void DegreeCalculate()
+	{
+		var difference = new Vector2 (pointList [0].x, pointList [0].y) - new Vector2 (pointList [1].x, pointList [1].y);
+		if (difference.y < Vector2.left.y) {
+			AngleArrange (difference,1);
+			pointList.Clear ();
+		} else {
+			AngleArrange (difference, -1);
+			pointList.Clear ();
 		}
 	}
+	void AngleArrange(Vector2 WhereTo,float InOrOut)
+	{
+		rot = Vector2.Angle (Vector2.left,WhereTo)*InOrOut;
+		var rot1 = Dot1.transform.rotation;
+		rot1.eulerAngles = new Vector3 (rot, -90, 90);
+		Dot1.transform.rotation = rot1;
+		var rot2 = Dot2.transform.rotation;
+		rot2.eulerAngles = new Vector3 (rot, -90, 90);
+		Dot2.transform.rotation = rot2;
+		var LineLaserTransform=Instantiate (LineLaser, DotPosition, Quaternion.identity) as GameObject;
+		var LineLaserRotation = LineLaserTransform.transform.rotation;
+		LineLaserRotation.eulerAngles = new Vector3 (0, 0, rot);
+		LineLaserTransform.transform.rotation = LineLaserRotation;
+	}
+
 }
